757803d40a0c1010acafb702e5f87c3436e45670
Vxypolytou <xypolytouv@gmail.com>
2024-05-04 08:36:17 -0400

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -1,65 +1,52 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Components
-import Budget from './components/Budget';
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
-import AllocationForm from './components/AllocationForm';
-
-import { AppProvider } from './context/AppContext';
+//costum components
+import { AppProvider } from "./context/AppContext";
+import Budget from "./components/Budget";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
+import Remaining from "./components/Remaining";
+import Currency from "./components/Currency";
 
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                        // Budget component
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>
-                        }        
-
-                        {
-                        //Remaining component
-                        <div className='col-sm'>
-                            <Remaining />
-                        </div>
-                        }        
-
-                        {
-                        //ExpenseTotal component
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>
-                        }        
-                       
-                        {
-                        <div className='col-sm'>
-                            <ExpenseList />
-                        </div>
-                        }         
-
-                        {
-                        <div className='col-sm'>
-                            <ExpenseItem />
-                        </div>
-                        }        
-
-                        {
-                        <div className='col-sm'>
-                            <AllocationForm />
-                        </div>
-                        }        
-
-                </div>
-            </div>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3 mb-4">Company's Budget Allocation</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <Remaining />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
+          <div className="col-sm">
+            <Currency />
+          </div>
+        </div>
+        <h3 className="mt-3">Expenses Table</h3>
+        <div style={{border:"2px solid #4e00ed4f"}} className="mb-4">
+        <div className="row ">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        </div>
+        <h3 className="mt-3">Change allocation</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AllocationForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
 };
+
 export default App;
